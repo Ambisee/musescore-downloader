eec58d4e5a90dcf82893196f5dd79b011b95e66a
Ambisee <celvin.weilington@gmail.com>
2023-09-11 23:42:06 -0700

    Added documentations and extra code for handling errors

--- musescore_downloader/core/save_pages.py
+++ musescore_downloader/core/save_pages.py
@@ -2,6 +2,7 @@ from logging import Logger
 
 from ..file_generation import PageSaver
 from ..managers import PathManager
+from ..common.exceptions.core import InvalidContentTypeError
 from ..common.types import (
     SaveCompleteObject, 
     ScoreScraperResult,
@@ -14,6 +15,25 @@ def save_pages(
     path_manager: PathManager,
     logger: Logger
 ) -> list[SaveCompleteObject] | Exception:
+    """Saves the content of each individual files into their own file on the filesystem.
+
+    Parameters
+    ----------
+    scraper_result : ScoreScraperResult
+        The object that contains the title of the score.
+    pf_scraper_result : list of ContentObject
+        The collection of objects that contains page number and the content of individual page.
+    path_manager : PathManager
+        The object that contains the paths to the filesystem to store the contents to.
+    logger : Logger
+        The object that handles printing messages.
+    
+    Returns
+    -------
+    list of SaveCompleteObject or Exception
+        If successful, the collection of objects that contains the path where each file is located.
+        Else, an exception detailing the error encountered.
+    """
     page_saver = PageSaver(
         scraper_result.title,
         pf_scraper_result,
@@ -22,6 +42,9 @@ def save_pages(
 
     try:
         result = page_saver.execute()
+    except KeyError as e:
+        logger.error(e)
+        return InvalidContentTypeError(str(e))
     except Exception as e:
         logger.error(e)
         return e
